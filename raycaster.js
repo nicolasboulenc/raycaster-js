02fefdfd60bb6b4a4eff672ca936f50dc2a2b559
Nicolas B. <nicolasboulenc@gmail.com>
2025-06-07 12:32:52 +0100

    yet another commit

--- raycaster.js
+++ raycaster.js
@@ -93,17 +93,17 @@ function loop(timestamp) {
 
 	// draw on cto
 
-	const rays = castrays()
+	const rays = castrays2()
 
 	if(draw_map) {
 		cto_map.fillStyle = "darkblue"
 		cto_map.fillRect(0, 0, cto_map.canvas.width, cto_map.canvas.height)
-		drawMap(rays)
+		drawMap2(rays)
 		ctx.drawImage(cto_map.canvas, 0, 0, cto_map.canvas.width, cto_map.canvas.height, 0, 0, canvas.width, canvas.height)
 	}
 	else {
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-		drawLevel(rays)
+		drawLevel2(rays)
 		ctx.drawImage(cto_render.canvas, 0, 0, cto_render.canvas.width, cto_render.canvas.height, 0, 0, canvas.width, canvas.height)
 	}
 }
@@ -116,19 +116,29 @@ function drawMap(rays) {
 	for(let y=0; y<map_h; y++) {
 		for(let x=0; x<map_w; x++) {
 			if(map[y * map_h + x] !== 0) {
-				cto_map.fillRect(x * map_s / scale + 1, y * map_s / scale + 1, map_s / scale - 1, map_s / scale - 1)
+				cto_map.fillRect(x * map_s / scale, y * map_s / scale, map_s / scale, map_s / scale)
 			}
 		}
 	}
+
+	// draw rays
+	cto_map.strokeStyle = "red"
+	cto_map.lineWidth = 2
+	cto_map.beginPath()
+	for(let ray of rays) {
+		cto_map.moveTo((player.x / scale), 	(player.y / scale))
+		cto_map.lineTo((Math.round(ray.x) / scale), 	(Math.round(ray.y) / scale))
+	}
+	cto_map.stroke()
 
 	// draw player
 	cto_map.fillStyle = "yellow"
 	cto_map.strokeStyle = "yellow"
 	cto_map.lineWidth = 1
 
-	let px = Math.round(player.x/scale)
-	let py = Math.round(player.y/scale)
-	const dir_length = 10
+	let px = Math.floor(player.x/scale)
+	let py = Math.floor(player.y/scale)
+	const dir_length = 16
 
 	cto_map.beginPath()
 	cto_map.moveTo(px - 0.5, py)
@@ -136,19 +146,88 @@ function drawMap(rays) {
 	cto_map.stroke()
 
 	cto_map.fillRect(px - 3, py - 3, 5, 5)
+}
+
+
+function drawMap2(rays) {
+	// draw map
+	cto_map.fillStyle = "darkgrey"
+	for(let y=0; y<map_h; y++) {
+		for(let x=0; x<map_w; x++) {
+			if(map[y * map_h + x] !== 0) {
+				cto_map.fillRect(x * map_s / scale, y * map_s / scale, map_s / scale, map_s / scale)
+			}
+		}
+	}
 
 	// draw rays
 	cto_map.strokeStyle = "red"
 	cto_map.lineWidth = 2
+	cto_map.beginPath()
 	for(let ray of rays) {
-
-		cto_map.beginPath()
-		cto_map.moveTo(Math.round(player.x / scale), 	Math.round(player.y / scale))
-		cto_map.lineTo(Math.round(ray.x / scale), 	Math.round(ray.y / scale))
-		cto_map.stroke()
+		cto_map.moveTo(player.x / scale, 		player.y / scale)
+		cto_map.lineTo(ray.x * map_s / scale, 	ray.y * map_s / scale)
 	}
+	cto_map.stroke()
+
+	// draw player
+	cto_map.fillStyle = "yellow"
+	cto_map.strokeStyle = "yellow"
+	cto_map.lineWidth = 1
+
+	let px = Math.floor(player.x / scale)
+	let py = Math.floor(player.y / scale)
+	const dir_length = 16
+
+	cto_map.beginPath()
+	cto_map.moveTo(px - 0.5, py)
+	cto_map.lineTo(px - 0.5 + Math.cos(player.dir) * dir_length, py + Math.sin(player.dir) * dir_length * -1)
+	cto_map.stroke()
+
+	cto_map.fillRect(px - 3, py - 3, 5, 5)
 }
 
+function drawLevel2(rays) { 
+
+	image_data_render.data.fill(0)
+	let x = 0
+	for(let ray of rays) {
+		let dst = ray.dst * Math.cos(player.dir - ray.a)
+		let h = Math.floor(1 / dst * res.h)
+		let y0 = Math.floor((res.h - h) / 2)
+		let y1 = y0 + h
+		// let color = { r: 100, g: 0, b: 0, a: 255 }
+
+		// if(ray.c === "v") {
+		// 	color = { r: 255, g: 0, b: 0, a: 255 }
+		// }
+
+		if(ray.a > player.dir - 0.0001 && ray.a < player.dir + 0.0001) {
+			console.log(ray.dst)
+		}
+
+		let texture_x = Math.floor(map_s * ray.u)
+		for(let y=y0; y<y1; y++) {
+
+			let v = (y - y0) / h
+			let texture_y = Math.floor(map_s * v)
+			let texture_i = texture_y * 64 + texture_x
+			let color = { 
+				r: image_data_texture.data[texture_i * 4 + 0],
+				g: image_data_texture.data[texture_i * 4 + 1],
+				b: image_data_texture.data[texture_i * 4 + 2],
+				a: image_data_texture.data[texture_i * 4 + 3]
+			}
+
+			image_data_render.data[ (y * res.w + x) * 4 + 0] = color.r
+			image_data_render.data[ (y * res.w + x) * 4 + 1] = color.g
+			image_data_render.data[ (y * res.w + x) * 4 + 2] = color.b
+			image_data_render.data[ (y * res.w + x) * 4 + 3] = color.a
+		}
+		x++
+	}
+	cto_render.putImageData(image_data_render, 0, 0)	
+}
 
 function drawLevel(rays) {
 
@@ -291,6 +370,133 @@ function castrays() {
 				ray_cur.x += ray_cur.x_step
 				ray_cur.y += ray_cur.y_step
 				ray_cur.dst = dir_cos * (ray_cur.x - player.x) - dir_sin * (ray_cur.y - player.y)
+			}
+			else {
+				// outside level?
+				ray_cur.dst = Infinity
+			}
+
+			if(ray_ver.has_hit && ray_ver.dst < ray_hor.dst) {
+				rays.push(ray_ver)
+				break
+			}
+			if(ray_hor.has_hit && ray_hor.dst < ray_ver.dst) {
+				rays.push(ray_hor)
+				break
+			}
+			if(ray_ver.dst >= max_dst && ray_hor.dst >= max_dst) {
+				// can this happen? if outside level?
+				rays.push(ray_ver)
+				break
+			}
+			// if(ray_ver.has_hit && ray_hor.has_hit) break;
+		}
+	}
+
+	return rays
+}
+
+
+function castrays2() {
+	const max_dst = Math.max(map_w, map_h)
+	const epsilon = 0.0001
+	let rays = []
+	let player_x = player.x / map_s
+	let player_y = player.y / map_s
+
+
+	for(let angle of angles) {
+
+		let ray_rad = player.dir - angle
+		let dir_tan = Math.tan(ray_rad)
+		let dir_inv_tan = 1.0 / dir_tan
+		let dir_cos = Math.cos(ray_rad)
+		let dir_sin = Math.sin(ray_rad)
+
+		// Ray that check vertical intersections
+		let ray_ver = {	a: ray_rad, x: 0, y: 0, x_step: 0, y_step: 0, dst: 0, has_hit: false, c: "v" }
+
+		if(dir_cos > epsilon) {
+			// looking left
+			ray_ver.x = Math.floor(player_x) + 1
+			ray_ver.y = (player_x - ray_ver.x) * dir_tan + player_y
+			ray_ver.x_step = 1
+			ray_ver.y_step = -ray_ver.x_step * dir_tan
+			ray_ver.dst = dir_cos * (ray_ver.x - player_x) - dir_sin * (ray_ver.y - player_y)
+		}
+		else if(dir_cos < -epsilon) {
+			// looking right
+			ray_ver.x = Math.floor(player_x) - epsilon
+			ray_ver.y = (player_x - ray_ver.x) * dir_tan + player_y
+			ray_ver.x_step = -1
+			ray_ver.y_step = -ray_ver.x_step * dir_tan
+			ray_ver.dst = dir_cos * (ray_ver.x - player_x) - dir_sin * (ray_ver.y - player_y)
+		}
+		else {
+			// looking up or down. no hit
+			ray_ver.x = player_x
+			ray_ver.y = player_y
+			ray_ver.dst = Infinity
+		}
+
+
+		// Ray that check vertical intersections
+		let ray_hor = {	a: ray_rad, x: 0, y: 0, x_step: 0, y_step: 0, dst: 0, has_hit: false, c: "h" }
+
+		if(dir_sin > epsilon) {
+			// looking up
+			ray_hor.y = Math.floor(player_y) - epsilon
+			ray_hor.x = (player_y - ray_hor.y) * dir_inv_tan + player_x
+			ray_hor.y_step = -1
+			ray_hor.x_step = -ray_hor.y_step * dir_inv_tan
+			ray_hor.dst = dir_cos * (ray_hor.x - player_x) - dir_sin * (ray_hor.y - player_y)
+		}
+		else if(dir_sin < -epsilon) {
+			//looking down
+			ray_hor.y = Math.floor(player_y) + 1
+			ray_hor.x = (player_y - ray_hor.y) * dir_inv_tan + player_x
+			ray_hor.y_step = 1
+			ray_hor.x_step = -ray_hor.y_step * dir_inv_tan
+			ray_hor.dst = dir_cos * (ray_hor.x - player_x) - dir_sin * (ray_hor.y - player_y)
+		}
+		else {
+			ray_hor.x = player_x
+			ray_hor.y = player_y
+			ray_hor.dst = Infinity
+		}
+
+
+		let ray_cur = ray_ver
+		while(true) {
+
+			if(ray_ver.has_hit) {
+				ray_cur = ray_hor
+			}
+			else if(ray_hor.has_hit) {
+				ray_cur = ray_ver
+			}
+			else if(ray_hor.dst < ray_ver.dst) {
+				ray_cur = ray_hor
+			}
+			else {
+				ray_cur = ray_ver
+			}
+
+			let map_x = Math.floor(ray_cur.x)
+			let map_y = Math.floor(ray_cur.y)
+			let map_idx = map_y * map_w + map_x
+
+			if(map_x >= 0 && map_x < map_w && map_y >= 0 && map_y < map_h && map[map_idx] !== 0) {
+				// hit
+				ray_cur.has_hit = true
+				ray_cur.dst = dir_cos * (ray_cur.x - player_x) - dir_sin * (ray_cur.y - player_y)
+				ray_cur.u = (ray_cur.x - Math.floor(ray_cur.x)) + (ray_cur.y - Math.floor(ray_cur.y))
+				// ray_cur.c = (map_x + map_y) % 2
+			}
+			else if(map_x >= 0 && map_x < map_w && map_y >= 0 && map_y < map_h) {
+				ray_cur.x += ray_cur.x_step
+				ray_cur.y += ray_cur.y_step
+				ray_cur.dst = dir_cos * (ray_cur.x - player_x) - dir_sin * (ray_cur.y - player_y)
 			}
 			else {
 				// outside level?
